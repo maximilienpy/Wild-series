aa4c80925d5b7944a39d5b6632483f4cbb6559a3
maximilienpy <max314116@gmail.com>
2021-02-24 21:19:32 +0100

    ajout en BDD des data, amélioration controller index et list pour utilisé les méthodes findBy et findOneBy, modification des view pour faire un affichage des séries par id

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,8 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
-
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
@@ -16,7 +17,7 @@ class ProgramController extends AbstractController
      * montre toutes les lignes de program's entity
      * 
      * 
-     * @Route("/programs", name="program_")
+     * @Route("/", name="index")
      * @return Response A response instance
      * 
      */
@@ -33,12 +34,26 @@ class ProgramController extends AbstractController
     }
 
     /** 
-     * @Route("/program/list/{id}/", methods={"GET"}, requirements={"id"="\d+"}, name="program_list")
+     * 
+     * Récupère un programme par son id
+     * 
+     * @Route("/list/{id<^[0-9]+$>}", name ="list")
+     * @return Response
+     * 
      */
-    public function show(int $id = 1): Response
+    public function list(int $id): Response
     {
-        return $this->render('program/list.html.twig', ['id' => $id]);
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('program/list.html.twig', [
+            'program' => $program,
+        ]);
     }
-
-
 }
