2f96239cbe1db60c4163f8e6e443c8ca7494295d
maximilienpy <max314116@gmail.com>
2021-02-25 03:34:45 +0100

    creation template show, lien entre les catégories et les series liées à cette catégorie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,19 +2,65 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Program;
+use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+* @Route("/categories", name="category_")
+*/
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category", name="category")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
+        //Récupérer toutes les catégories de la BDD
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories,
+        ]);
+    }
+
+    /** 
+     * 
+     * Récupère une catégory par
+     * 
+     * @Route("/show/{categoryName}", name ="show")
+     * @return Response
+     * 
+     */
+    public function show(string $categoryName): Response
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findBy(['name' => $categoryName]);
+
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(
+                ['category' => $category],
+                ['id' => 'DESC'],
+                3
+            );
+
+        
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'La catégorie : '.$categoryName.' n\'a pas été trouvé.'
+            );
+        }
+        return $this->render('category/show.html.twig', [
+            'categoryName' => $categoryName,
+            'program' => $program
         ]);
     }
 }
